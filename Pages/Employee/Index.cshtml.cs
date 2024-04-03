867f390b489fa878db07bea2b90e421d5c47c482
Giraneza <giraneza.jeandedieu@gmail.com>
2024-04-03 15:40:43 +0200

    workin on Employee

---
+++ Pages/Employee/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace flight_management_system.Pages.Employee
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Employee/Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace flight_management_system.Pages.Employee
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
