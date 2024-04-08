70adbe61d9a4e62f0d9549ce7b389ca1ea7b7e7c
Giraneza <giraneza.jeandedieu@gmail.com>
2024-04-08 13:21:26 +0200

    Booking search functionality

---
+++ Pages/Booking/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace flight_management_system.Pages.Booking
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
